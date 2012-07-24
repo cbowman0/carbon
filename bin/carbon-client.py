7325d865802e2603af2bfecd97610f7b29489693
Christopher Bowman <cbowman0@gmail.com>
2012-07-24 20:48:25 +0000

    Add in a file loader

--- bin/carbon-client.py
+++ bin/carbon-client.py
@@ -49,6 +49,8 @@ option_parser = OptionParser(usage="%prog [options] <host:port:instance> <host:p
 option_parser.add_option('--debug', action='store_true', help="Log debug info to stdout")
 option_parser.add_option('--keyfunc', help="Use a custom key function (path/to/module.py:myFunc)")
 option_parser.add_option('--datadir', help="Directory containing files to load", default=None)
+option_parser.add_option('--datafile-prefix', help="Load files which start with this prefix, ignore the rest", default='')
+option_parser.add_option('--pidfile', help="Create a pidfile upon launch", default=None)
 option_parser.add_option('--replication', type='int', default=1, help='Replication factor')
 option_parser.add_option('--routing', default='consistent-hashing',
   help='Routing method: "consistent-hashing" (default) or "relay"')
@@ -114,7 +116,9 @@ class StdinMetricsReader(LineReceiver):
       (metric, value, timestamp) = line.split()
       datapoint = (float(timestamp), float(value))
       assert datapoint[1] == datapoint[1] # filter out NaNs
-      client_manager.sendDatapoint(metric, datapoint)
+      while not client_manager.sendDatapoint(metric, datapoint):
+        log.msg("Failed to send datapoint, retrying")
+        time.sleep(0.1)
     except:
       log.err(None, 'Dropping invalid line: %s' % line)
 
@@ -134,47 +138,113 @@ class StdinMetricsReader(LineReceiver):
 
 
 class FileLoader(object):
-  def __init__(self, datadir):
+  def __init__(self, datadir, datafile_prefix=''):
     self.filelist = []
     self.datadir = datadir
+    self.datafile_prefix = datafile_prefix
+    self.current_file = None
+    self.sleep_for = 0
+    self.lines_to_reinject = []
+
+  def backoff(self, cycles):
+    self.sleep_for = cycles
+
+  def in_backoff(self):
+    if not bool(self.sleep_for):
+      return False
+    time.sleep(0.01)
+    if self.sleep_for > 1:
+      self.sleep_for -= 1
+      return True
+    lines_to_reinject = self.lines_to_reinject
+    self.lines_to_reinject = []
+    for line in lines_to_reinject:
+      self.sendDatapoint(line)
+    self.sleep_for -= 1
+    return False
 
   def getNextFile(self):
-    if len(self.filelist) == 0:
-      self.filelist = os.listdir(self.datadir)[-100:]
+    while len(self.filelist) == 0:
+      self.filelist = [f for f in os.listdir(self.datadir) if (os.path.splitext(f)[1] == '.log') and f.startswith(self.datafile_prefix) ][0:100]
       if len(self.filelist) < 10:
         # relax if there is not that much data
         time.sleep(1)
     return os.path.join(self.datadir, self.filelist.pop())
 
-  def loadMetricsFile(self):
+  def acquireLock(self, file):
     try:
-      fname = self.getNextFile()
-      if os.path.splitext(fname)[1] == '.log':
-        log.msg("Feeding %s" % fname)
-        f = open(fname)
-        while True:
-          line = f.readline()
-          if not line:
-            break
+      os.link(file, "%s.lock" % file)
+    except:
+      return False
+    else:
+      return True
+
+  def releaseLock(self, file):
+    try:
+      os.remove("%s.lock" % file)
+    except:
+      return False
+    else:
+      return True
+
+  def sendDatapoint(self, line):
+    try:
+      (metric, value, timestamp) = line.strip().split("\t")
+      metric = metric.strip().replace(' ', '_')
+      datapoint = (float(timestamp), float(value))
+      if not client_manager.sendDatapoint(metric, datapoint):
+        if len(self.lines_to_reinject) > 20:
+          self.backoff(4)
+          log.msg("Failed to send datapoints: backing off and retrying, current metric:%s" %  metric)
+        self.lines_to_reinject.append(line)
+    except:
+      log.err(None, 'Dropping invalid line: %s' % line)
+
+  def loadDataFile(self):
+    try:
+      if self.current_file is None:
+        fname = self.getNextFile()
+        if os.path.splitext(fname)[1] != '.log':
+          return
+        if self.acquireLock(fname):
+          log.msg("Feeding %s" % fname)
           try:
-            (metric, value, timestamp) = line.strip().split()
-            datapoint = (float(timestamp), float(value))
-            client_manager.sendDatapoint(metric, datapoint)
+            self.current_file = open(fname)
           except:
-            log.err(None, 'Dropping invalid line: %s' % line)
-        f.close()
-        try:
-          os.remove(fname)
-        except OSError:
-          pass
+            #XXX Log this.
+            return
+        else:
+          return
+      while True:
+        if self.in_backoff():
+          return
+        line = self.current_file.readline()
+        if not line:
+          break
+        self.sendDatapoint(line)
+      self.current_file.close()
+      try:
+        os.remove(self.current_file.name)
+        self.releaseLock(self.current_file.name)
+        self.current_file = None
+      except OSError:
+        self.current_file = None
+        pass
     except IndexError:
-      log.debug("No data, sleeping 1 sec")
+      log.debug("No data, time.sleeping 1 sec")
       time.sleep(1)
- 
+
+  def sendDatapoint(self, *args, **kwargs):
+    raise TypeError, "sendDatapoint is an abstract method"
+
 if options.datadir is None:
   stdio.StandardIO( StdinMetricsReader() )
 else:
-  fl = FileLoader(options.datadir)
+  fl = FileLoader(options.datadir, options.datafile_prefix)
+  if options.pidfile is not None:
+    pid_f = open(options.pidfile, 'w')
+    pid_f.write(str(os.getpid()) + "\n")
+    pid_f.close()
   lc = LoopingCall(fl.loadMetricsFile)
   lc.start(0)
 
